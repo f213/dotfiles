dd6d814a92b63e3708ad1919689246e05ef83e4f
Fedor Borshev <f@f213.in>
2018-07-07 18:45:07 +0300

    More verbose installation process

--- install-brew-stuff.sh
+++ install-brew-stuff.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
+set -e
 [ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 [ -z $(which fish) ] && brew install fish
 [ -z $(which hub) ] && brew install hub
+
+exit 0
