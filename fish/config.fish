32e3fdc639e94a8662ac4f0dd7ceb24f519b1a1c
Fedor Borshev <f@f213.in>
2018-07-07 18:01:41 +0300

    Fish options update

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,7 @@ fish_vi_key_bindings
 
 set -x LC_ALL en_US.UTF-8
 set -x VIRTUAL_ENV_DISABLE_PROMPT off
-set PATH ~/bin /usr/local/bin/ /Library/Frameworks/Python.framework/Versions/2.7/bin $PATH
+set PATH ~/bin /usr/local/bin/ $PATH
 
 function fish_mode_prompt; end
-set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
 
