354b465f026fc765ed4da62abc40d733f59cdaca
Fedor Borshev <f@f213.in>
2018-07-28 17:24:10 +0300

    Moved scraper1 call to the env

--- fish/functions/scraper1.fish
+++ fish/functions/scraper1.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
-function scraper1 --description 'alias scraper1 scrapyc --url http://scraper1.rumetr.com:6800 --username root --password ***REMOVED***'
-	scrapyc --url http://scraper1.rumetr.com:6800 --username root --password ***REMOVED*** $argv;
+function scraper1
+	scrapyc --url http://scraper1.rumetr.com:6800 --username $SCRAPER1_USERNAME --password $SCRAPER1_PASSWORD $argv;
 end
