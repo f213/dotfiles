ec003caf3f6e6f5e3551f86980b9cd4166d0b8ae
Fedor Borshev <f@f213.in>
2018-07-28 17:17:50 +0300

    Added load of my private env variables

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,11 @@ fish_vi_key_bindings
 
 set -x LC_ALL en_US.UTF-8
 set -x VIRTUAL_ENV_DISABLE_PROMPT off
+
 set PATH ~/bin /usr/local/bin/ $PATH
 
+export (cat ~/.fish.env|xargs -L 1)  # set private environment variables
+
 function fish_mode_prompt; end
+
 
