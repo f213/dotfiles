7ada15cd03548969b2b0fb9094a57095d9048591
Fedor Borshev <fedor@borshev.com>
2020-04-05 13:01:26 +0300

    Moved fish installation on top for running oh-my-fish

--- install-brew-stuff.sh
+++ install-brew-stuff.sh
@@ -4,5 +4,6 @@ set -e
 [ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 [ -z $(which ansible) ] && brew install ansible
+[ -z $(which fish) ] && brew install fish
 
 exit 0
