06f64e1fad1330df775b00d3fafa9817b8c89542
Fedor Borshev <fedor@borshev.com>
2020-04-11 13:29:21 +0000

    Ad-hoc improvements

--- install-brew-stuff.sh
+++ install-brew-stuff.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 set -e
-[ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-[ -z $(which ansible) ] && brew install ansible
-[ -z $(which fish) ] && brew install fish
+if [ "$OSTYPE" == "darwin"* ]; then
+  [ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  [ -z $(which ansible) ] && brew install ansible
+  [ -z $(which fish) ] && brew install fish
+
+else
+  sudo apt -y install ansible
+fi
+
 
 exit 0
