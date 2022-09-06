f4d9f64cb89d1863748a200421ca523dcb3b1e60
Fedor Borshev <fedor@borshev.com>
2022-09-06 20:30:08 +0300

    fish: dropped stale aliases

--- fish/f213/rmpycache.fish
+++
@@ -1,3 +0,0 @@
-# clean python compiler cache, usefull for experiments with docker and such
-
-alias rmpycache="find . -type d -name '__pycache__'|xargs rm -Rf; and find . -type f -name '*.pyc'|xargs rm"
--- fish/f213/scraper1.fish
+++
@@ -1,4 +0,0 @@
-# this my personal alias, non needed for you, sorry
-function scraper1
-    scrapyc --url http://scraper1.rumetr.com:6800 --username                    $SCRAPER1_USERNAME --password $SCRAPER1_PASSWORD $argv;
-end
