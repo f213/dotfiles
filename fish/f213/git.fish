376a3cec9d1b824188bbfc79f1b8ec0a5e61842d
Fedor Borshev <fedor@borshev.com>
2022-10-21 18:11:01 +0300

    Gitignore moved to to dedicated command

--- fish/f213/git.fish
+++
@@ -1,7 +0,0 @@
-# Defined in - @ line 0
-function git --description 'alias git=hub'
-	hub  $argv;
-end
-
-abbr g git
-abbr gpoat git push origin --all
---
+++ fish/f213/gitignore.fish
@@ -0,0 +1,3 @@
+function gitignore
+	curl -sL https://www.toptal.com/developers/gitignore/api/$argv
+end
