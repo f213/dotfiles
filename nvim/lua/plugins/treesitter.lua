02bfdc6a4419ae29b2e36b2e067f68c56e4cdc3c
Fedor Borshev <fedor@borshev.com>
2025-03-27 22:57:08 +0600

    Make treesitter conf inline with the docs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function ()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed = { "python", "javascript", "lua", "vim", "htmldjango", "html", "jinja", "json", "xml", "yaml",  },
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = { "python", "javascript", "lua", "vim", "htmldjango", "html", "jinja", "json", "xml", "yaml",  },
 
-        highlight = {
-          enable = true,
-        },
-      }
-    end,
+      highlight = {
+        enable = true,
+      },
+    }
 }
