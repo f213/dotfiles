bcd5e62576e15c0e4148a05605dc75e844325a7a
Fedor Borshev <fedor@borshev.com>
2025-12-17 19:12:35 +0300

    nvim: disabled automatic poping up of the autocomplete menu

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,11 +12,13 @@ vim.opt.hlsearch = false
 vim.diagnostic.config({
   underline = false,
 })
-vim.opt.autocomplete = true
-vim.opt.completeopt = { "menu", "menuone", "preview", "preinsert" }
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 
 vim.cmd('filetype plugin indent on')
 
 vim.keymap.set('n', '<C-T>', '<CMD>tab new<CR>')
 vim.keymap.set('n', '<Leader>ff', vim.lsp.buf.format)
+vim.keymap.set('i', '<C-F>', function()
+  if vim.fn.pumvisible() == 1 then return '<C-N><C-Y>' end
+end, { expr = true })
--- nvim/lsp/jedi.lua
+++ nvim/lsp/jedi.lua
@@ -2,7 +2,9 @@ return {
   cmd = { 'pipx', 'run', '-q', 'jedi-language-server' },
   filetypes = { 'python' },
   on_attach = function(client, bufnr)
-    vim.lsp.completion.enable(true, client.id, bufnr)
+    vim.lsp.completion.enable(true, client.id, bufnr, {
+      autotrigger = true,
+    })
   end,
   settings = {
     hover = {
